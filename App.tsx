6eb2b3dbedcb67b0cf3faa1602e4b6cd9d670a6f
suhailalturaabgroup <suhail.a@alturaabgroup.com>
2021-06-03 20:00:15 +0530

    changes

--- App.tsx
+++ App.tsx
@@ -3,21 +3,32 @@ import { NavigationContainer, DrawerActions, StackActions, } from '@react-naviga
 import { StyleSheet, Text, Button, View, TouchableOpacity } from 'react-native';
 import { createStackNavigator, StackScreenProps } from '@react-navigation/stack';
 import { StackTabParamList, RootStackParamList } from './types';
-import Home from './components/Home';
 import About from './components/About';
 import TabNav from './navigation/TabNav';
-
+import { Ionicons } from '@expo/vector-icons';
 const RootStack = createStackNavigator();
 
 
 
-function App() {
+function App({navigation}:StackScreenProps<RootStackParamList>) {
 
   return (
 
      <NavigationContainer>
-       <RootStack.Navigator>
-       <RootStack.Screen name="Home" component={TabNav} />
+       <RootStack.Navigator
+          screenOptions={{
+
+            headerLeft: () => 
+            <TouchableOpacity onPress={()=> navigation.dispatch(DrawerActions.openDrawer())}>
+              
+              <Ionicons name="menu" size={32} color="green" />
+            </TouchableOpacity>
+          }}
+       >
+       <RootStack.Screen name="Home" component={TabNav}
+          
+         
+       />
        <RootStack.Screen name="About" component={About} />
        </RootStack.Navigator>
 
--- components/Home.tsx
+++ components/Home.tsx
@@ -1,17 +1,16 @@
 import * as React from 'react';
 import { Button, TouchableOpacity, StyleSheet, View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator, StackScreenProps } from '@react-navigation/stack';
 import { RootStackParamList } from '../types';
 import { Ionicons } from '@expo/vector-icons'; 
-
+import { NavigationContainer, DrawerActions, StackActions, } from '@react-navigation/native';
 function Home({ navigation }: StackScreenProps<RootStackParamList, 'NotFound'>) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
       
-     
-        <Button title="click" onPress={() => navigation.navigate('About')}/>
+       <Button title="open" onPress={()=> navigation.dispatch(DrawerActions.openDrawer())} />
+        {/* <Button title="click" onPress={() => navigation.navigate('NotFound')}/> */}
    
     </View>
   );
